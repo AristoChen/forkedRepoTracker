5de83a3ade44485bc07577c26b1b1f1102dccf29
AristoChen <jj251510319013@gmail.com>
2020-04-04 13:11:21 +0800

    Use github API and remove BeautifulSoup

--- forkedRepoTracker.py
+++ forkedRepoTracker.py
@@ -1,6 +1,9 @@
-from bs4 import BeautifulSoup
+# -*- coding: utf-8 -*-
+
 import requests
 import getopt
+import json
+import time
 import sys
 import re
 
@@ -9,16 +12,22 @@ def usage():
 	print "-h / --help: print this help message"
 	print "-u / --url: specify the github url"
 	print "-v / --verbose: print more message"
-	print "\nExample: python test.py -u https://github.com/AUTHOR/REPO"
+	print "\nExample: python forkedRepoTracker.py -u https://github.com/AUTHOR/REPO"
 
-def get(url):
-	res = requests.get(url)
+def get(url, username = "", token = ""):
+	res = requests.get(url, auth=(username, token))
 	while res.status_code != requests.codes.ok:
-		print "[Error] http response code: " + str(res.status_code) + ", retrying"
-		res = requests.get(url)
+		print "[Error] url: {0}\n http response code: {1}, response body: {2}".format(url, str(res.status_code), res.text)
+		if res.status_code == 404:
+			break
+		res = requests.get(url, auth=(username, token))
 	return res
 
 if __name__ == "__main__":
+	if len(sys.argv) < 4 or "-u" not in sys.argv:
+		usage()
+		sys.exit(2)
+
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "hu:v", ["help", "url=", "verbose"])
 	except getopt.GetoptError as err:
@@ -38,62 +47,101 @@ if __name__ == "__main__":
 				print "Invalid url: " + url
 				usage()
 				sys.exit(2)
-			if url.find("https://") == -1:
-				url = "https://" + url
 			if url[-1] != '/':
 				url = url + '/'
+			authorOriginal = url[url[:url[:-1].rfind('/')-len(url)].rfind('/')+1:url[:-1].rfind('/')]
 			repoName = url[url[:-1].rfind('/')+1:-1]
-			url = url + "network/members"
 		else:
 			assert False, "Invalid option"
 
-	res = get(url)
-	soup = BeautifulSoup(res.text, 'html.parser')
-	userList = soup.find_all("div", {"class": "repo"})
+	with open('userInfo.json', 'r') as file:
+		userInfo = json.loads(file.read())
+		username = userInfo["username"]
+		token = userInfo["token"]
+		if username == "" or token == "":
+			print "Please fill in your github account info"
 
-	if len(userList) > 1:
-		print "Found " + str(len(userList)-1) + " forked repo"
+	baseURL = "https://api.github.com/"
 
-		for i in range(len(userList)):
-			if i == 0:
-				continue
-			sys.stdout.write("Processing: %d / %d\r" % (i, len(userList)-1))
+	rateLimitURL = "{0}rate_limit".format(baseURL)
+	res = get(rateLimitURL, username, token)
+	rateLimitInfo = json.loads(res.text)
+	remain = rateLimitInfo["resources"]["core"]["remaining"]
+	resetTime = time.ctime(rateLimitInfo["resources"]["core"]["reset"])
+	print "Remaining resources: {0}, and will be reset at: {1}".format(remain, resetTime)
+	if remain == 0:
+		print "Not enough resources, please wait until reset"
+		sys.exit(1)
+
+	page = 1
+	forkList = []
+	while True:
+		forkListURL = "{0}repos/{1}/{2}/forks?page={3}".format(baseURL, authorOriginal, repoName, str(page))
+		res = get(forkListURL, username, token)
+		forkListTmp = json.loads(res.text)
+
+		if len(forkListTmp) == 0:
+			print "\nAll forked repo fetched"
+			break
+		else:
+			sys.stdout.write("Processing fork list page:  %d\r" % page)
 			sys.stdout.flush()
-			userName = userList[i].find('a')['href'][1:]
-			url = "https://github.com/" + userName + "/" + repoName
-			res = get(url)
-			soup = BeautifulSoup(res.text, 'html.parser')
+			forkList += forkListTmp
+		page += 1
 
-			branchInfoBar = soup.find_all("div", {"class": "branch-infobar"})[0].text
-			branchInfo = re.findall(r'.*(This branch.*master.).*', branchInfoBar)[0]
+	print "Found " + str(len(forkList)) + " forked repo"
 
-			if branchInfo.find("ahead") != -1:
-				print "Author: " + userName + ", " + branchInfo
+	for i in range(len(forkList)):
+		sys.stdout.write("Processing: %d / %d\r" % (i+1, len(forkList)))
+		sys.stdout.flush()
 
-				aheadCommits = int(re.findall(r'([0-9]+) commits? ahead', branchInfo)[0])
-				url = "https://github.com/" + userName + "/" + repoName + "/commits/master"
-				res = get(url)
-				soup = BeautifulSoup(res.text, 'html.parser')
+		authorFork = forkList[i]["owner"]["login"]
+		authorParent = authorOriginal
+		compareURL = "{0}repos/{1}/{2}/compare/{3}:master...{1}:master".format(baseURL, authorFork, repoName, authorParent)
+		res = get(compareURL, username, token)
+		compareResult = json.loads(res.text)
+		try:
+			compareStatus = compareResult["status"]
+			aheadCommits = compareResult["ahead_by"]
+			behindCommits = compareResult["behind_by"]
+		except KeyError:
+			repoURL = "{0}repos/{1}/{2}".format(baseURL, authorFork, repoName)
+			res = get(repoURL, username, token)
+			repoResult = json.loads(res.text)
+			try:
+				authorParent = repoResult["parent"]["owner"]["login"]
+				compareURL = "{0}repos/{1}/{2}/compare/{3}:master...{1}:master".format(baseURL, authorFork, repoName, authorParent)
+				res = get(compareURL, username, token)
+				compareResult = json.loads(res.text)
+				try:
+					compareStatus = compareResult["status"]
+					aheadCommits = compareResult["ahead_by"]
+					behindCommits = compareResult["behind_by"]
+				except KeyError:
+					print "Warning: {0} seems to be not exists, index: {1}".format(authorFork, str(i))
+			except KeyError:
+				print "Warning: {0} seems to be not exists, index: {1}".format(authorFork, str(i))
+				continue
 
-				commitList = soup.find_all("li", {"class": "commit"})
-				if len(commitList) < aheadCommits:
-					commitsPerPage = len(commitList)
-					latestCommit = soup.find("clipboard-copy")["value"]
-					for i in range(aheadCommits/commitsPerPage):
-						url = "https://github.com/" + userName + "/" + repoName + "/commits/master?after=" + latestCommit + "+" + str(commitsPerPage*(i+1)-1)
-						print url
-						res = get(url)
-						soup = BeautifulSoup(res.text, 'html.parser')
-						commitList += soup.find_all("li", {"class": "commit"})
+		if aheadCommits > 0:
+			print "Author: {0}, {1} commits ahead and {2} commits behind of {3}:master".format(authorFork, aheadCommits, behindCommits, authorParent)
 
-				for i in range(aheadCommits):
-					commitMessage = commitList[i].find_all("a", {"class": "message"})[0].text.strip()
-					commitTime = commitList[i].find("relative-time")["datetime"]
+			page = 1
+			commitsList = []
+			while True:
+				commitURL = "{0}repos/{1}/{2}/commits?page={3}".format(baseURL, authorFork, repoName, str(page))
+				res = get(commitURL, username, token)
+				commitsList += json.loads(res.text)
 
-					print "\tCommit message: " + commitMessage + ", time: " + commitTime
-			else:
-				if verbose == True:
-					print "Author: " + userName + ", " + branchInfo
+				if len(commitsList) >= aheadCommits:
+					break
+			for j in range(aheadCommits):
+				try:
+					print "\t{0}".format(str(j) + ". " + commitsList[j]["commit"]["message"].encode('utf-8').replace("\n", "\n\t   "))
+				except UnicodeEncodeError:
+					print "Encoding error, index: {0}".format(str(i))
+		else:
+			if verbose == True:
+				print "Author: {0}, {1} commits ahead and {2} commits behind of {3}:master".format(authorFork, aheadCommits, behindCommits, authorParent)
 
-	else:
-		print "Can not find any forked repo"
+	print "\nCompleted"
